b450cb04abb13b3d44afcd813fe6ab0321288798
ivanov-i <e@mail.mail>
2014-10-04 12:48:04 +0700

    Kill all chromes

---
+++ KillChrome/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let kill (p: System.Diagnostics.Process) =
+    p.Kill()
+    ()
+
+let protect f =
+    fun p ->
+        try
+            f p
+        with _ -> ()
+
+let protectedKill = protect(kill)
+
+[<EntryPoint>]
+let main argv = 
+    System.Diagnostics.Process.GetProcessesByName("chrome")
+    |> Array.iter (fun p -> protectedKill p)
+    0 // return an integer exit code
